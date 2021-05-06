bb623f2369d1ca4ec5b59ea3c47c6f19b569812f
Vitoraugustorosa <augustovitor909@gmail.com>
2021-05-06 16:03:33 -0300

    While

---
+++ src/br/com/vitor/LacoWhile.java
@@ -0,0 +1,16 @@
+package br.com.vitor;
+
+public class LacoWhile {
+
+	public static void main(String[] args) {
+		int valor = 0;
+		System.out.println("Valor" + valor);
+		
+		while(valor <= 25) {
+			System.out.println("-> Valor:" + valor);
+			valor = valor + 1;
+		}
+		System.out.println("Agora valor é: " + valor);
+	}
+
+}
